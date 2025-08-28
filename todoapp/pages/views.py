1b2c4dfb220c9390c51a0178b30369e0650030d5
CJ <christianjherardilagan@gmail.com>
2025-08-28 23:58:28 +0800

    models form

--- todoapp/pages/views.py
+++ todoapp/pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-from .models import Task
+from .models import *
+from .forms import *
 # Create your views here.
 def index(request, *args, **kwargs):
     task = Task.objects.all()
-    context = {'tasks': task}
+    context = {'tasks': task, 'form': forms}
 
     return render(request, 'home.html', context)
