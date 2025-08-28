56b3c211e672b653219a218cd95044355d1b7797
CJ <christianjherardilagan@gmail.com>
2025-08-29 00:58:52 +0800

    edit function added

--- todoapp/pages/views.py
+++ todoapp/pages/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import *
-from .forms import *
+from .models import Task
+from .forms import TaskForm
 # Create your views here.
 def index(request, *args, **kwargs):
     task = Task.objects.all()
-    context = {'tasks': task, 'form': forms}
+    context = {'tasks': task, 'form': TaskForm()}
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid(): 
+            form.save()
+            context['form'] = TaskForm()
+        return redirect('/')
 
     return render(request, 'home.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    context = {'form': form}
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    return render(request, 'update.html', context)
